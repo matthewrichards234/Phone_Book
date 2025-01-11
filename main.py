c8ac492c0afe08e3532a5602af8e589bcf95ffee
Matthew Richards <matthewrichards234@gmail.com>
2025-01-11 13:11:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,38 @@ def display_menu():
     print("3. Search Contact")
     print("4. Exit")
 
-def add_contacts():
-    return
+def add_contacts(contacts):
+    name = input("Please enter the contact's name: ").title()
+    phone = input("Enter their phone number: ")
+    address = input("Enter their address: ")
+    favorite = bool(input("Favorite this contact (T/F): "))
+    contacts[name] = [phone, address, favorite]
 
-def display_contacts():
-    return
+def display_contacts(contacts):
+    if not contacts:
+        print("You have not added any contacts yet. Please add to your contact list.\n")
+    else:
+        for contact in contacts:
+            print(contact)
 
-def search_contacts():
+def search_contacts(contacts):
     return
 
 def main():
-    return
+    contacts = {} # Name : Phone, Address, Favorite(T/F)
+
+    menu = display_menu()
+    print(menu)
+
+    option = input("What would you like to do: ")
+
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option == 4:
+        exit
+    else:
+        print("Invalid option. Please try again.\n")
